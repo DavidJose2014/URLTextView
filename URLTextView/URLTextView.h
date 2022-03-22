1cfb2a62dca09395037ce0f15ab56f7f45c8d7ec
Vinu David Jose <vinud@qburst.com>
2022-03-22 14:22:19 +0530

    Initial Commit

---
+++ URLTextView/URLTextView.h
@@ -0,0 +1,18 @@
+//
+//  URLTextView.h
+//  URLTextView
+//
+//  Created by Vinu David Jose on 22/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for URLTextView.
+FOUNDATION_EXPORT double URLTextViewVersionNumber;
+
+//! Project version string for URLTextView.
+FOUNDATION_EXPORT const unsigned char URLTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLTextView/PublicHeader.h>
+
+
